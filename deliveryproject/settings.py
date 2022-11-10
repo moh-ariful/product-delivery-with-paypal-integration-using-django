44ea438f78191e1a24262b59cc618eba4c284bf9
Moh Ariful <106416095+moh-ariful@users.noreply.github.com>
2022-11-10 11:53:35 +0700

    Update settings.py

--- deliveryproject/settings.py
+++ deliveryproject/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jq2&m=%g$$hou@trq_oi!*$ipem!!j_gw)b#p!@*&url0p*v$p'
+SECRET_KEY = 'django-insecure-jq2&m=%g$$hou@trq_xxxxxxxxxxxxxxxx)xxxxxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
